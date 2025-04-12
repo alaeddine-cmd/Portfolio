714c7499556787b30faebd5579594669ebd42bc0
alaeddine-cmd <alaeddine_12@yahoo.fr>
2025-04-13 00:26:53 +0100

    change view

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,7 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DevelopperController;
-use App\Http\Controllers\ExperienceController;
 
 Route::get('/', [DevelopperController::class, 'fetch_me']);
-
-
 Route::get('/profile', [DevelopperController::class, 'fetch_me'])->name('layouts.profile');
 Route::post('/contact', [ContactController::class, 'send'])->middleware('throttle:3,1')->name('contact.send');
