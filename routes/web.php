22ab242ffe75291dc4959ee313c47ea7f1d38cde
alaeddine-cmd <alaeddine_12@yahoo.fr>
2025-04-12 22:27:53 +0100

    symlink

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@ Route::get('/', function () {
 
 Route::get('/profile', [DevelopperController::class, 'fetch_me'])->name('layouts.profile');
 Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
-Route::get('/create-symlink', function (){
-    symlink(storage_path('/app/public'), public_path('storage'));
-    echo "Symlink Created. Thanks";
-});
+
 
 
